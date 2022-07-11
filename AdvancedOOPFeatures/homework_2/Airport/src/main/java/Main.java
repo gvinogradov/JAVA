8d512c4cda459fa855aaa6234c65c496aa48459d
Georgiy Vinogradov <dev626@yandex.ru>
2022-07-11 22:00:56 +0300

    13.12.2 homework done stream filter & max

--- AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
+++ AdvancedOOPFeatures/homework_2/Airport/src/main/java/Main.java
@@ -1,17 +1,38 @@
+import com.skillbox.airport.Aircraft;
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
+import com.skillbox.airport.Terminal;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-
-
+        Airport airport = Airport.getInstance();
+        List<Flight> flightList = findPlanesLeavingInTheNextTwoHours(airport);
+        flightList.forEach(System.out::println);
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
-        //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
+        return  airport.getTerminals().stream()
+                .collect(Collectors.toMap(Function.identity(), Terminal::getFlights)).values()
+                .stream().collect(Collectors.toList())
+                .stream().flatMap(e -> e.stream())
+                .filter(f -> inTwoHours(f.getDate()) && f.getType().equals(Flight.Type.DEPARTURE))
+                .collect(Collectors.toList());
+    }
+
+    public static boolean inTwoHours(Date date) {
+        Date dateNow = Date.from(LocalDateTime.now()
+                .atZone(ZoneId.systemDefault()).toInstant());
+        Date dateInTwoHours = Date.from(LocalDateTime.now().plusHours(2)
+                .atZone(ZoneId.systemDefault()).toInstant());
+        return (date.compareTo(dateNow) >= 0 && date.compareTo(dateInTwoHours) <= 0);
     }
 
 }
