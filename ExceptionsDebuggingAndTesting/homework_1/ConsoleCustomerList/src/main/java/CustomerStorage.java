5cfcc74671f8c72a6714c6a594cbee48d77f5c02
Georgiy Vinogradov <dev626@yandex.ru>
2022-07-17 15:07:13 +0300

    14.10.1 homework added unchecked exceptions throws

--- ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/CustomerStorage.java
+++ ExceptionsDebuggingAndTesting/homework_1/ConsoleCustomerList/src/main/java/CustomerStorage.java
@@ -1,8 +1,24 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CustomerStorage {
     private final Map<String, Customer> storage;
+
+    public static final Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
+    public static boolean validateMail(String emailStr) {
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(emailStr);
+        return matcher.find();
+    }
+
+    public static final Pattern VALID_TEL_REGEX = Pattern.compile("\\+[7-8]?[0-9]{10}", Pattern.CASE_INSENSITIVE);
+
+    public static boolean validateTel(String telStr) {
+        Matcher matcher = VALID_TEL_REGEX.matcher(telStr);
+        return matcher.find();
+    }
 
     public CustomerStorage() {
         storage = new HashMap<>();
@@ -13,8 +29,10 @@ public class CustomerStorage {
         final int INDEX_SURNAME = 1;
         final int INDEX_EMAIL = 2;
         final int INDEX_PHONE = 3;
-
         String[] components = data.split("\\s+");
+        if (components.length != 4) throw new IllegalArgumentException("Illegal argument count");
+        if (!validateMail(components[INDEX_EMAIL])) throw new IllegalArgumentException("Wrong e-mail format");
+        if (!validateTel(components[INDEX_PHONE])) throw new IllegalArgumentException("Wrong phone format");
         String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
         storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
     }
